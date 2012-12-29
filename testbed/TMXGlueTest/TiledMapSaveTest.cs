75e45f99f6ecc8901656f4a17b790dc2d098dc12
kainazzzo <ddatti@gmail.com>
2012-12-29 00:08:26 -0500

    Added a new unit test for tileset properties

--- testbed/TMXGlueTest/TiledMapSaveTest.cs
+++ testbed/TMXGlueTest/TiledMapSaveTest.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 
 namespace TMXGlueTest
 {
-    
-    
+
+
     /// <summary>
     ///This is a test class for TiledMapSaveTest and is intended
     ///to contain all TiledMapSaveTest Unit Tests
@@ -21,6 +21,7 @@ namespace TMXGlueTest
         public TestContext TestContext { get; set; }
 
         #region Additional test attributes
+
         // 
         //You can use the following additional attributes as you write your tests:
         //
@@ -48,6 +49,7 @@ namespace TMXGlueTest
         //{
         //}
         //
+
         #endregion
 
 
@@ -76,7 +78,10 @@ namespace TMXGlueTest
 
             target.layer[1].name = "layer2";
 
-            var expected = "Name (required),name2,name1,name3\r\nlayer1,\"val2\",\"val\",\r\nlayer2,,\"val\",\"val3\"\r\n"; // TODO: Initialize to an appropriate value
+
+            var expected =
+                "Name (required),name2,name1,name3\r\nlayer1,\"val2\",\"val\",\r\nlayer2,,\"val\",\"val3\"\r\n";
+                // TODO: Initialize to an appropriate value
             var actual = target.ToCSVString(type, null);
             Assert.AreEqual(expected, actual);
 
@@ -84,9 +89,147 @@ namespace TMXGlueTest
             expected = "Name (required),name2,name1\r\nlayer1,\"val2\",\"val\"\r\n";
             actual = target.ToCSVString(type, layerName);
             Assert.AreEqual(expected, actual);
+
+
         }
 
         /// <summary>
+        ///A test for ToCSVString
+        ///</summary>
+        [TestMethod()]
+        public void ToCSVStringTilesetPropertyTest()
+        {
+            var target = new TiledMapSave();
+            const TiledMapSave.CSVPropertyType type = TiledMapSave.CSVPropertyType.Tile;
+            target.tileset = new mapTileset[2]
+                {
+                    new mapTileset()
+                        {
+                            Firstgid = 1,
+                            Name = "tileset1",
+                            Tile = new List<mapTilesetTile>()
+                                {
+                                    new mapTilesetTile()
+                                        {
+                                            id = 1,
+                                            properties = new List<property>()
+                                                {
+                                                    new property()
+                                                        {
+                                                            name = "commonPropertyName",
+                                                            value = "commonPropertyValueTileset1Tile1"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "tileset1Tile1PropertyName",
+                                                            value = "tileset1Tile1PropertyValue"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "name",
+                                                            value = "tileset1Tile1"
+                                                        }
+                                                }
+                                        },
+                                    new mapTilesetTile()
+                                        {
+                                            id = 2,
+                                            properties = new List<property>()
+                                                {
+                                                    new property()
+                                                        {
+                                                            name = "name",
+                                                            value = "tileset1Tile2"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "commonPropertyName",
+                                                            value = "commonPropertyValueTileset1Tile2"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "tileset1Tile2PropertyName",
+                                                            value = "tileset1Tile2PropertyValue"
+                                                        }
+                                                }
+                                        },
+                                },
+                        },
+                    new mapTileset()
+                        {
+                            Firstgid = 3,
+                            Name = "tileset2",
+                            Tile = new List<mapTilesetTile>()
+                                {
+                                    new mapTilesetTile()
+                                        {
+                                            id = 1,
+                                            properties = new List<property>()
+                                                {
+                                                    new property()
+                                                        {
+                                                            name = "name",
+                                                            value = "tileset2Tile1"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "commonPropertyName",
+                                                            value = "commonPropertyValueTileset2Tile1"
+                                                        },
+                                                    new property()
+                                                        {
+                                                            name = "tileset2Tile1PropertyName",
+                                                            value = "tileset2Tile1PropertyValue"
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                };
+            target.layer = new mapLayer[1]
+                {
+                    new mapLayer()
+                        {
+                            height = 32,
+                            width = 96,
+                            name = "layer1",
+                            visible = 1,
+                            data = new mapLayerData[1]
+                                {
+                                    new mapLayerData()
+                                        {
+                                            dataTiles = new mapLayerDataTile[3]
+                                                {
+                                                    new mapLayerDataTile()
+                                                        {
+                                                            gid = "1"
+                                                        },
+                                                    new mapLayerDataTile()
+                                                        {
+                                                            gid = "2"
+                                                        },
+                                                    new mapLayerDataTile()
+                                                        {
+                                                            gid = "3"
+                                                        }
+                                                }
+                                        }
+                                },
+                        }
+                };
+            target.orientation = "Orthogonal";
+            target.tileheight = 32;
+            target.tilewidth = 32;
+
+            string expected =
+                "Name (required),tileset1Tile1PropertyName,commonPropertyName,tileset1Tile2PropertyName,tileset2Tile1PropertyName\r\ntileset1Tile1,\"tileset1Tile1PropertyValue\",\"commonPropertyValueTileset1Tile1\",,\r\ntileset1Tile2,,\"commonPropertyValueTileset1Tile2\",\"tileset1Tile2PropertyValue\",\r\ntileset2Tile1,,\"commonPropertyValueTileset2Tile1\",,\"tileset2Tile1PropertyValue\"\r\n";
+            string actual = target.ToCSVString(type: TiledMapSave.CSVPropertyType.Tile, layerName: null);
+            Assert.AreEqual(expected, actual);
+        }
+    
+
+
+    /// <summary>
         ///A test for BuildPropertyDictionaryConcurrently
         ///</summary>
         [TestMethod()]
